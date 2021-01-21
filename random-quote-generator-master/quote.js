cb49cf6804b22f49937bc5d6fc92bfc99e11310b
RebeccaKanu <kanurebecca0@gmail.com>
2021-01-21 22:58:57 +0100

    ..

---
+++ random-quote-generator-master/quote.js
@@ -0,0 +1,10 @@
+let button = document.getElementById('buttons');
+
+let child = button.childNodes;
+
+child[0].style.fontWeight = 'bold';
+
+/*let p1 = document.getElementById('p1');
+  let enfantsP1 = p1.childNodes;
+  enfantsP1[1].style.fontWeight = 'bold';*/
+
